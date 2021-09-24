6735b7a29b3d9eacabf21d8b7d29360ec47ea379
zmoon <zmoon92@gmail.com>
2021-09-23 22:09:18 -0600

    Split off Key into its own module

--- pyabc2/test_note.py
+++ pyabc2/test_note.py
@@ -1,9 +1,9 @@
 """
-Test keys module
+Test the note module
 """
 import pytest
 
-from pyabc2.keys import Key, Pitch, PitchClass, pitch_value
+from pyabc2.note import Pitch, PitchClass, pitch_value
 
 
 @pytest.mark.parametrize("p", ["C", "Dbb"])
@@ -89,49 +89,6 @@ def test_eq():
 def test_nice_names_from_values():
     ps = [PitchClass.from_value(i) for i in range(6)]
     assert [p.name for p in ps] == ["C", "C#", "D", "Eb", "E", "F"]
-
-
-def many_possible_key_name_inputs():
-    # https://github.com/campagnola/pyabc/blob/4c22a70a0f40ff82f608ffc19a1ca51a153f8c24/tests/test_keys.py#L11-L42
-    from itertools import product
-
-    from pyabc2.keys import CHROMATIC_NOTES, MODE_VALUES
-
-    # Base list of keys as C chromatic notes
-    keys = CHROMATIC_NOTES
-
-    # List of modes in sentence case
-    modes = [m.capitalize() for m in MODE_VALUES]
-
-    # Append upper and lower case versions of the above
-    modes += [m.lower() for m in modes] + [m.upper() for m in modes]
-
-    # Append truncated versions of the above
-    modes += [m[:3] for m in modes]
-    modes += ["m"]
-
-    return [k + m for k, m in product(keys, modes)]
-
-
-@pytest.mark.parametrize("key_name", many_possible_key_name_inputs())
-def test_parse_key_basic_succeeds(key_name):
-    # TODO: could create a shorter version with a few selected examples and mark this one as long
-    # Attempt to create key using key string provided.
-    Key(name=key_name)
-
-
-def test_key_sig():
-    k = Key("Amaj")
-    assert k.key_signature == ["F#", "C#", "G#"]
-    for n in "FCG":
-        assert k.accidentals[n] == "#"
-
-
-def test_relatives():
-    Am = Key("Am")
-    C = Key("C")
-    assert Am.relative_ionian == Am.relative_major == C
-    assert C.relative_aeolian == C.relative_minor == Am
 
 
 @pytest.mark.parametrize(("note", "octave", "expected_freq"), [("C", 4, 261.6256), ("A", 4, 440.0)])
