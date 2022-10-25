b88a7582642d272c74a4ba30f9e796fb09df3d18
zmoon <zmoon92@gmail.com>
2022-10-24 21:08:37 -0600

    Cleanup
    importlib.metadata available in all supported Pythons now

--- tests/test_pyabc2.py
+++ tests/test_pyabc2.py
@@ -1,25 +1,17 @@
 """
 Test top-level
 """
-try:
-    from importlib.metadata import metadata  # type: ignore[import]
-except ModuleNotFoundError:
-    im_avail = False
-else:
-    im_avail = True
-    pyabc2_metadata = metadata("pyabc2")
+from importlib.metadata import metadata  # type: ignore[import]
 
-import pytest
+pyabc2_metadata = metadata("pyabc2")
 
 import pyabc2
 
 
-@pytest.mark.skipif(not im_avail, reason="no importlib.metadata")
 def test_version():
     assert pyabc2.__version__ == pyabc2_metadata["version"]
 
 
-@pytest.mark.skipif(not im_avail, reason="no importlib.metadata")
 def test_short_description_consistency():
     module_descrip = pyabc2.__doc__.strip().split("\n")[0]
 
