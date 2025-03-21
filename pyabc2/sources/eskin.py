a2e108e2031c31fb8f4e0fe0516b2777d8b4f37f
zmoon <zmoon92@gmail.com>
2025-03-21 14:30:17 -0500

    Download/store data
    working for kss at least...

--- pyabc2/sources/eskin.py
+++ pyabc2/sources/eskin.py
@@ -3,9 +3,14 @@ Load data from the Eskin tunebook websites.
 """
 
 import json
+from pathlib import Path
 from urllib.parse import parse_qs, urlsplit
 
 from pyabc2.sources._lzstring import LZString
+
+HERE = Path(__file__).parent
+
+SAVE_TO = HERE / "_eskin"
 
 _TBW = "https://michaeleskin.com/tunebook_websites"
 _CCE_SD = "https://michaeleskin.com/cce_sd"
@@ -31,23 +36,65 @@ s = """\
     ";"
 )
 
-_, s = s.split(" ", 1)
-type_, s = s.split("=", 1)
 
-try:
-    data = json.loads(s)
-except json.JSONDecodeError as e:
-    print(s[e.pos], "context:", s[e.pos - 10 : e.pos + 10])
-    raise
-
-for d in data[:1]:
-    name = d["Name"]
-    abctools_url = d["URL"]
-    res = urlsplit(abctools_url)
+def abctools_url_to_abc(url: str) -> str:
+    """Extract the ABC from an abctools share URL."""
+    res = urlsplit(url)
     assert res.netloc == "michaeleskin.com"
-    query_params = parse_qs(res.query)
-    (lzw,) = query_params["lzw"]  # note `+` has been replaced with space
+    assert res.path.startswith("/abctools/")
 
-    # Example LZW string (made by js `LZString.compressToEncodedURIComponent()`):
-    # 'BoLgBAjAUApDAuBLeAbApgMwPYDt5gAUBDFIpHLMAJithgGcBXAIyVU132NL0QsgAcdPtmx5CJMn0oQALFAAqIAII4wAWSwiAFmkQAnKACUQRtGhRQA8iACS i0RwATKAGUQ6xvUQBjMBj6WAC2YPaOLmAAalgoAHRgAMxQAEKe3n4BQaHhPM7RsQnJAFogCvpO9L76iAAOSLjg6ogA1mhgADK4AOZQ6iAAwlAdIBAA9EIA0iAAIrAGiPTaAPpBKCj0y7iwRMy AFab9FiMLmL4GGTMWACedOq2M7ZgtUHdFaEADPePz8xE9Horyw3TA3zgDyeYF82iw mcwNB4JgkL APoADdYmAAKzI1HQ2HwrEoXHfABEM3JzioYGUAHEwAAxZRgGZUAA ygANDMwAA-dlgZy Bkcynk5T5fk0sCU4KSgC8GDQznFA3Jit8tPp-gAor5dRyADpQCWyhnM5QzFLcrm8gVCkViqBSgUW5ws9Xk mKgZ6xkSpkzdkgE1QDkgc20jD5IjOS7e7pa1Vy5TkgB6opSvnFVJltLdLJmTI5Zvp5P59NpLLlGq1tJz4YlboLdITasl5N8zkwYG6Me6HP5RFpaCI0KIaFz5IAFPSAJTU0WBqm10McoA'
-    text = LZString.decompressFromEncodedURIComponent(lzw)
+    query_params = parse_qs(res.query)
+    (lzw,) = query_params["lzw"]
+    # Note `+` has been replaced with space by parse_qs
+    # js LZString.compressToEncodedURIComponent() is used to compress/encode the ABC
+
+    # TODO: optionally remove the lines that start with % or at least the %% lines
+
+    return LZString.decompressFromEncodedURIComponent(lzw)
+
+
+def _download_data(tunebook_url: str):
+    """Extract and save the tune data from the tunebook webpage as JSON."""
+    import gzip
+    import re
+
+    import requests
+
+    r = requests.get(tunebook_url, timeout=5)
+    r.raise_for_status()
+    html = r.text
+
+    # First find the tune type options by searching for 'tunes = type;'
+    types = sorted(set(re.findall(r"tunes = (.*?);", html)))
+    if not types:
+        raise RuntimeError("Unable to detect tune types")
+    print(types)
+
+    # Then the data are in like `const reels=[{...}, ...];`
+    all_data = {}
+    for type_ in types:
+        m = re.search(f"const {type_}=(.*?);", html, flags=re.DOTALL)
+        if m is None:
+            raise RuntimeError(f"Unable to find data for type {type_!r}")
+        s_data = m.group(1)
+
+        try:
+            data = json.loads(s_data)
+        except json.JSONDecodeError as e:
+            print(s_data[e.pos], "context:", s_data[e.pos - 10 : e.pos + 10])
+            raise
+
+        for d in data:
+            assert d.keys() == {"Name", "URL"}
+            d["name"] = d.pop("Name")
+            d["abc"] = abctools_url_to_abc(d.pop("URL"))
+
+        all_data[type_] = data
+
+    SAVE_TO.mkdir(exist_ok=True)
+    stem = Path(urlsplit(tunebook_url).path).stem
+    with gzip.open(SAVE_TO / f"{stem}.json.gz", "wt") as f:
+        json.dump(all_data, f, indent=2)
+
+
+_download_data(_TBWS["kss"])
