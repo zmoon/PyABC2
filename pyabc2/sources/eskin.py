c9252cf51b112c03ecaf056a4a5026ba037555ff
zmoon <zmoon92@gmail.com>
2025-03-21 15:03:32 -0500

    Data loader; remove certain lines when downloading
    kss is %-free now

--- pyabc2/sources/eskin.py
+++ pyabc2/sources/eskin.py
@@ -3,7 +3,9 @@ Load data from the Eskin tunebook websites.
 """
 
 import json
+import re
 from pathlib import Path
+from typing import Literal, Tuple
 from urllib.parse import parse_qs, urlsplit
 
 from pyabc2.sources._lzstring import LZString
@@ -28,17 +30,33 @@ _TBWS = {
     "playford3": f"{_TBW}/playford_3_partington_20jan2025.html",
 }
 
-# https://michaeleskin.com/tunebook_websites/king_street_sessions_tunebook_17Jan2025.html
-# reels line extract
-s = """\
-    const reels=[{"Name":"An Moinfheir","URL":"https://michaeleskin.com/abctools/abctools.html?lzw=BoLgBAjAUApDAuBLeAbApgMwPYDt5gAUBDFIpHLMAJithgGcBXAIyVU132NL0QsgAcdPtmx5CJMn0oQALFAAqIAII4wAWSwiAFmkQAnKACUQRtGhRQA8iACS+i0RwATKAGUQ6xvUQBjMBj6WAC2YPaOLmAAalgoAHRgAMxQAEKe3n4BQaHhPM7RsQnJAFogCvpO9L76iAAOSLjg6ogA1mhgADK4AOZQ6iAAwlAdIBAA9EIA0iAAIrAGiPTaAPpBKCj0y7iwRMy+AFab9FiMLmL4GGTMWACedOq2M7ZgtUHdFaEADPePz8xE9Horyw3TA3zgDyeYF82iw+mcwNB4JgkL+APoADdYmAAKzI1HQ2HwrEoXHfABEM3JzioYGUAHEwAAxZRgGZUAA+ygANDMwAA-dlgZy+Bkcynk5T5fk0sCU4KSgC8GDQznFA3Jit8tPp-gAor5dRyADpQCWyhnM5QzFLcrm8gVCkViqBSgUW5ws9Xk+mKgZ6xkSpkzdkgE1QDkgc20jD5IjOS7e7pa1Vy5TkgB6opSvnFVJltLdLJmTI5Zvp5P59NpLLlGq1tJz4YlboLdITasl5N8zkwYG6Me6HP5RFpaCI0KIaFz5IAFPSAJTU0WBqm10McoA&format=noten&ssp=10&name=An_Moinfheir&play=1"},{"Name":"Around the world","URL":"https://michaeleskin.com/abctools/abctools.html?lzw=BoLgBATAUApDAuBLeAbApgMwPYDt5gAUBDFIpHLSaOAZwFcAjJVTXfY0vRCsARgA5YMbtmx5CJMt0q8ALFAAqIAIIAnLHRwATMPAAWaMAHcsqlFsUgFB44hqGAtkUQWASiFdo0KKAHkQAJKq3kTaUADKgap2emAKqkRayIi4JGAAsnQ0iADGYOFoNNm4cZqFUABCUTFxCUlIqSgZWbn5hcU4pTjlAFpWCTg0OdEADg04ADRgRKoDAOZoDmh4Uzl6plo0IOmIANaGADK4c1DpIADCUAcgvAD0ggDSIAAisIjRNHoA+uooKDRfXCwIgMHIAKwBNA02jE+AwZAYWAAnkJ0gFngEwCN1HMEg4wAAGVHozEMIhFbFYOaE4kYsBrDaU6lEuBoulkooANywTQArCyYGzMQzVFpuXyiQAiZ6SgBiEFlAHEwLKAKLPVUAH1lymUFTAyjVz010slWggYAAFABmS2YOYASjAkuUkowWjQORNMq0FUNztdFVV6s1AB0oKb5UqVeqtTq9QajWGIz6LTa7RhHQG3R6vVAANq8Z2Ks1+5WR54akCa-MWyUl33Kcsy2XPHXhzUgU3mpsqtvKb1m+EYbNzDBoOaDjDKLQjl2StDKHJoTUpodoHTznK6iDJ+uSioQRX603KfuD2dEOeuscbk0l9MVHJaJ3L52W54O0tNmtFyOu11VVlZ0ZWeBVlGrWsQLlADJVVZtpQgDVNQAXSAA&format=noten&ssp=10&name=Around_the_world&play=1"}];
-""".strip().rstrip(
-    ";"
-)
 
+def abctools_url_to_abc(
+    url: str,
+    *,
+    remove_prefs: str | Tuple[str] | Literal[False] = (
+        r"%%titlefont ",
+        r"%%subtitlefont ",
+        r"%%infofont ",
+        r"%irish_rolls_on",
+        r"%abcjs_",
+        r"%%MIDI ",
+    ),
+) -> str:
+    """Extract the ABC from an Eskin abctools share URL.
 
-def abctools_url_to_abc(url: str) -> str:
-    """Extract the ABC from an abctools share URL."""
+    Parameters
+    ----------
+    remove_prefs
+        Remove lines starting with these prefixes.
+        Use ``False`` or an empty iterable to keep all lines instead.
+    """
+
+    if not remove_prefs:
+        remove_prefs = ()
+    elif isinstance(remove_prefs, str):
+        remove_prefs = (remove_prefs,)
+
     res = urlsplit(url)
     assert res.netloc == "michaeleskin.com"
     assert res.path.startswith("/abctools/")
@@ -48,15 +66,18 @@ def abctools_url_to_abc(url: str) -> str:
     # Note `+` has been replaced with space by parse_qs
     # js LZString.compressToEncodedURIComponent() is used to compress/encode the ABC
 
-    # TODO: optionally remove the lines that start with % or at least the %% lines
+    abc = LZString.decompressFromEncodedURIComponent(lzw)
 
-    return LZString.decompressFromEncodedURIComponent(lzw)
+    wanted_lines = [
+        line.strip() for line in abc.splitlines() if not line.lstrip().startswith(remove_prefs)
+    ]
+
+    return "\n".join(wanted_lines)
 
 
 def _download_data(tunebook_url: str):
     """Extract and save the tune data from the tunebook webpage as JSON."""
     import gzip
-    import re
 
     import requests
 
@@ -68,7 +89,6 @@ def _download_data(tunebook_url: str):
     types = sorted(set(re.findall(r"tunes = (.*?);", html)))
     if not types:
         raise RuntimeError("Unable to detect tune types")
-    print(types)
 
     # Then the data are in like `const reels=[{...}, ...];`
     all_data = {}
@@ -97,4 +117,11 @@ def _download_data(tunebook_url: str):
         json.dump(all_data, f, indent=2)
 
 
-_download_data(_TBWS["kss"])
+def _load_data(key: str):
+    """Load the data from the saved JSON."""
+    import gzip
+
+    stem = Path(urlsplit(_TBWS[key]).path).stem
+    fp = SAVE_TO / f"{stem}.json.gz"
+    with gzip.open(fp, "rt") as f:
+        return json.load(f)
