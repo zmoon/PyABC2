a165f5609a795a5468821d6551106b1797023ae1
zmoon <zmoon92@gmail.com>
2026-01-16 22:52:36 -0600

    Test in_jupyter

--- tests/test_pyabc2.py
+++ tests/test_pyabc2.py
@@ -7,6 +7,7 @@ from importlib.metadata import metadata  # type: ignore[import]
 pyabc2_metadata = metadata("pyabc2")
 
 import pyabc2
+from pyabc2._util import in_jupyter
 
 
 def test_version():
@@ -18,3 +19,7 @@ def test_short_description_consistency():
     module_descrip = pyabc2.__doc__.strip().split("\n")[0]
 
     assert module_descrip == pyabc2_metadata["summary"]
+
+
+def test_in_jupyter():
+    assert not in_jupyter()
